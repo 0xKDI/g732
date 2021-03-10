f8ed9472f84eb45c2e96762329c74e5be0aa3e7a
qq <0qqw0qqw@gmail.com>
2021-03-10 09:47:40 +0300

    add latexmkrc

---
+++ latexmkrc.rb
@@ -0,0 +1,6 @@
+$xelatex = "xelatex --shell-escape %O %S";
+$pdf_mode = 5;
+$interaction = "nonstopmode";
+$preview_continuous_mode = 1;
+$clean_ext = "_minted-%R/* _minted-%R";
+# $pdf_previewer = "zathura %S";
